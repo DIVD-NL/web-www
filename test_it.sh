05bf836d6000089bc4b43b8f2f080795bebe1212
Frank Breedijk <f.breedijk@divd.nl>
2022-11-08 16:11:30 +0100

    Make test_it.sh work with the new github-pages container

--- test_it.sh
+++ test_it.sh
@@ -5,10 +5,6 @@ docker run --volume="$PWD:/root/project:delegated" --entrypoint /bin/bash -ti mr
 -c "
 	set -e
 	cd /root/project/;
-	apt update -y
-	apt install python3-pip libcurl4 -y
-	pip3 install html5validator 
-	gem install html-proofer
 	bundle install
 	git config --global --add safe.directory /root/project/csirt.divd.nl
 	./update.sh;
